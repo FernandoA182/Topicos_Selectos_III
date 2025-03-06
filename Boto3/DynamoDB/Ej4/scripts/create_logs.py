f068cccc8178d58adb0852ad84780393ca1d994e
Fernando Arevalo <ferndoarturo182@gmail.com>
2025-03-05 20:32:35 -0600

    Update with my routes

--- Boto3/DynamoDB/Ej4/scripts/create_logs.py
+++ Boto3/DynamoDB/Ej4/scripts/create_logs.py
@@ -4,12 +4,29 @@ from datetime import datetime
 
 
 def create_logs(sample_size=10):
-    df = pd.read_csv('../data/OnlineRetail.csv', encoding = "ISO-8859-1")
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    csv_path = os.path.join(script_dir, '..', 'data', 'OnlineRetail.csv')
+    print("CSV path:", csv_path)
+    
+    df = pd.read_csv(csv_path, encoding="ISO-8859-1")
     df_sample = df.sample(n=sample_size)
 
     name = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 
-    df_sample.to_json(f'../logs/{name}.log', orient="records")
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    
+    # Ruta absoluta de la carpeta logs
+    logs_dir = os.path.join(script_dir, '..', 'logs')
+    
+    # Si la carpeta logs no existe, crearla:
+    if not os.path.exists(logs_dir):
+        os.makedirs(logs_dir)
+        
+    # Construye la ruta completa al archivo
+    log_path = os.path.join(logs_dir, f'{name}.log')
+    
+    # Guardar como JSON
+    df_sample.to_json(log_path, orient="records")
 
 
 
